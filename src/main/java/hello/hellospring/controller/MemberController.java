f399c7b3ff3fe392bc97805146d75ebc8fa02592
jsungl <wotjd931026@gmail.com>
2023-04-03 00:17:52 +0900

    check to enrolled member name

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -31,7 +31,7 @@ public class MemberController {
 
         Member member = new Member();
         member.setName(form.getName());
-//        System.out.println("member = " + member.getName() );
+        System.out.println("member = " + member.getName() );
         memberService.join(member);
 
         return "redirect:/";
